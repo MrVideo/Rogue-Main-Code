2eb430a9ce365d8a6571646e344e8bb02d4fb3b6
Mario Merlo <m20_11@icloud.com>
2020-12-04 14:53:24 +0100

    Removed snippets.c and fixed map printing in the map loader

--- mapLoader.c
+++ mapLoader.c
@@ -5,10 +5,16 @@
 int main()
 {
     FILE *map = fopen("map1.txt", "r");
-    int dim, i, j;
-    char currentLine[100], *currentLinePointer;
+    int dim, i, j, k = 0;
+    char currentLine[100], mapMatrix[100][100];
 
-    currentLinePointer = calloc(100, sizeof(char));
+    for(i = 0; i < 100; i++)
+    {
+        *(currentLine + i) = '\0';
+
+        for(j = 0; j < 100; j++)
+            mapMatrix[i][j] = '\0';
+    }
 
     if(map == NULL)
     {
@@ -18,17 +24,30 @@ int main()
 
     fscanf(map, "%d", &dim);
 
-    fseek(map, sizeof(char), SEEK_CUR);
+    fseek(map, sizeof(char) * 3, SEEK_SET);
 
     for(i = 0; i < dim * 2; i++)
     {   
-        fgets(currentLine, dim * 2 + 1, map);
-
-        for(j = 0; j < dim * 2; j++)
+        if(!feof(map))
         {
-            printf("%c", *(currentLine + j));
+            fgets(currentLine, dim * 2 + 1, map);
+
+            for(j = 0; j < dim * 2 + 1; j++)
+            {
+                printf("%c", *(currentLine + j));
+            }
         }
     }
+
+    //Debug
+    for(i = 0; i < dim * 2 + 1; i++)
+    {
+        for(j = 0; j < dim * 2 + 1; j++)
+            printf("%c", mapMatrix[i][j]);
+        
+        printf("\n");
+    }
+    //Debug end
 
     printf("\n");
 
