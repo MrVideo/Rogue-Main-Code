c947629865dd65b393a8c421647749b01815a2d1
Mario Merlo <m20_11@icloud.com>
2020-12-05 16:32:25 +0100

    Fixed segmentation fault. Weird values in matrix.

--- main.c
+++ main.c
@@ -2,53 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
-int **mapLoader(int *size, char *filename);
+void loadMap(int **mapMatrix, int *size, char *filename);
 
 int main()
 {
-    int dim, i, j, mapMatrix[100][100], **mapPtr, *size;
-    char filename[30];
+    char filename[20];
+    int size = 0, *sizePtr, **mapMatrix, i, j;
 
-    size = &dim;
-    mapPtr = mapMatrix;
-
+    mapMatrix = calloc(100, sizeof(int));
     for(i = 0; i < 100; i++)
+        mapMatrix[i] = calloc(100, sizeof(int));
+
+    //Debug    
+    for(i = 0; i < size; i++)
     {
-        for(j = 0; j < 100; j++)
-            mapMatrix[i][j] = '\0';
-    }
-
-    system("clear");
-
-    printf("Inserisci il nome del file contenente la mappa: ");
-    scanf("%s", filename);
-
-    mapPtr = mapLoader(size, &filename[0]);
-
-    //Debug
-    for(i = 0; i < dim; i++)
-    {
-        for(j = 0; j < dim; j++)
-            printf("%d", mapMatrix[i][j]);
+        for(j = 0; j < size; j++)
+            printf("%d ", mapMatrix[i][j]);
         
         printf("\n");
     }
+
+    printf("\nPrimo elemento della matrice: %d\n", mapMatrix[0][0]);
     //Debug end
+
+    sizePtr = &size;
     
-    return 0;
+    printf("Inserisci il nome della mappa: ");
+    scanf("%s", filename);
+
+    loadMap(&mapMatrix[0], sizePtr, filename);
+
+    //Debug
+    for(i = 0; i < size; i++)
+    {
+        for(j = 0; j < size; j++)
+            printf("%d ", mapMatrix[i][j]);
+        
+        printf("\n");
+    }
+
+    printf("\nPrimo elemento della matrice: %d\n", mapMatrix[0][0]);
+    //Debug end
 }
 
-int **mapLoader(int *size, char *filename)
+void loadMap(int **mapMatrix, int *size, char *filename)
 {
     FILE *map = fopen(filename, "r");
-    int i, j, mapMat[100][100];
     char currentLine[100];
-
-    if(map == NULL)
-    {
-        printf("File error: file doesn't exist\n");
-        return 0;
-    }
+    int i, j;
 
     fscanf(map, "%d", size);
 
@@ -64,14 +65,17 @@ int **mapLoader(int *size, char *filename)
             for(j = 0; j < *size * 2; j++)
             {
                 if(currentLine[j] != ' ')
-                {   
-                    mapMat[i][j] = (int)currentLine[j];
+                {
+                    mapMatrix[i][j] = (int)currentLine[j];
+                    //Debug
+                    printf("%c ", currentLine[j]);
+                    //Debug end
                 }
             }
+
+            //Debug
+            printf("\n");
+            //Debug end
         }
     }
-
-    fclose(map);
-
-    return &mapMat[0][0];
 }
